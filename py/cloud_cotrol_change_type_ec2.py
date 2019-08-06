c62a2bc2a2b92ae7ea16a6d2050bb1d416189a98
Pawel Piwosz <pawel@pawelpiwosz.net>
2019-08-06 23:27:15 +0200

    initial commit

---
+++ py/cloud_cotrol_change_type_ec2.py
@@ -0,0 +1,44 @@
+""" Lambda function - change ec2 type """
+import boto3
+
+def cloud_control_change_type_ec2(event, context):
+    """ Lambda function - change ec2 type """
+
+    # validate instance name
+    ec2 = boto3.resource('ec2')
+    ec2_client = boto3.client('ec2')
+    response = ec2_client.describe_instances(
+        Filters=[
+            {
+                'Name': 'tag:Name',
+                'Values': [event["body"]["InstanceName"]]
+            }
+        ]
+    )
+    instance_list = []
+    for reservation in response['Reservations']:
+        for instance in reservation['Instances']:
+            instance_list.append(instance['InstanceId'])
+
+    if not instance_list:
+        msg = "I cannot find the instance with name {}.".format(event["body"]["InstanceName"])
+        return {"msg": msg}
+
+    ec2_instance = ec2.instances.filter(InstanceIds=instance_list)
+
+    try:
+        ec2_client.stop_instances(InstanceIds=[ec2_instance])
+        if_stopped = ec2_client.get_waiter('instance_stopped')
+        if_stopped.wait(InstanceIds=[ec2_instance])
+        ec2_client.modify_instance_attribute(
+            InstanceId=ec2_instance,
+            Attribute='instanceType',
+            Value=event["body"]["NewType"])
+        ec2_client.start_instances(InstanceIds=[ec2_instance])
+        msg = "Instance is back on-line."
+        return {"msg": msg}
+    except (SyntaxError, KeyError, NameError) as e_msg:
+        msg = "An error occured during resizing! The reason is {}".format(
+            str(e_msg)
+        )
+        return {"msg": msg}
